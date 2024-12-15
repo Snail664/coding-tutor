d8407896f63211a7d9ad64bc6b62f95970a488d0
Chiraag Anand <chiraag_anand_from.tp@imda.gov.sg>
2024-12-15 14:23:38 +0800

    use markdown for llm feedback

--- components/features/Assistant/LLMFeedback.tsx
+++ components/features/Assistant/LLMFeedback.tsx
@@ -1,10 +1,14 @@
+import Markdown from "react-markdown";
+
 // LLMFeedback.tsx
 export default function LLMFeedback({ LLMResponse }: { LLMResponse: string }) {
   return (
     <div className="flex-[2]">
       <p className="font-bold text-md">LLM Assistant Feedback:</p>
       <div className="flex-1 bg-menuBackground p-3 rounded-md min-h-[100px] flex flex-col">
-        <div className="flex-1 text-sm overflow-auto p-1">{LLMResponse}</div>
+        <div className="flex-1 text-sm overflow-auto p-1">
+          <Markdown>{LLMResponse}</Markdown>
+        </div>
       </div>
     </div>
   );
