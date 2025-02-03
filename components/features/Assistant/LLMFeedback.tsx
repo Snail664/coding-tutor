066485750dbe0dc942f1c9c2e2c02d0ec20f86d2
Chiraag Anand <chiraagsinghanand@gmail.com>
2025-02-03 10:41:23 +0800

    ui enhancements for llm feedback

--- components/features/Assistant/LLMFeedback.tsx
+++ components/features/Assistant/LLMFeedback.tsx
@@ -1,9 +1,19 @@
 import Markdown from "react-markdown";
 
 // LLMFeedback.tsx
-export default function LLMFeedback({ LLMResponse }: { LLMResponse: string }) {
+export default function LLMFeedback({
+  userQuestion,
+  LLMResponse,
+}: {
+  userQuestion: string;
+  LLMResponse: string;
+}) {
   return (
     <div className="flex-[2]">
+      <p className="font-bold text-md">User Question:</p>
+      <div className="flex-1 bg-menuBackground p-3 rounded-md mb-5 flex flex-col">
+        <div className="flex-1 text-sm overflow-auto p-1">{userQuestion}</div>
+      </div>
       <p className="font-bold text-md">LLM Assistant Feedback:</p>
       <div className="flex-1 bg-menuBackground p-3 rounded-md min-h-[100px] flex flex-col">
         <div className="flex-1 text-sm overflow-auto p-1">
